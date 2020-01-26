a54019a47956912c4076cf11125e585ed763d604
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-01-26 17:05:25 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < Sinatra::Base
 
 	configure do
 		set :views, "app/views"
-		# enable :sessions
-		# set :session_secret, "password_security"
+		enable :sessions
+		set :session_secret, "password_security"
 	end
 
 	get "/" do
