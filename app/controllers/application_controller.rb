5540192243978a20b50452789ade4891cd414eb4
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2020-01-26 17:28:15 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,7 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/login" do
-	    binding.pry
+	    #binding.pry
 		#your code here!
 		  user = User.find_by(:username => params[:username])
          if user && user.authenticate(params[:password])
